47d05ffb0bfd5ae5fa2565758411dc6df1f42b35
Huấn Nguyễn Danh <huannd@kaopiz.com>
2024-05-06 18:10:27 +0700

    fix-route

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::
     ->middleware('auth:api');
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
+Route::post('/updateUser',[AuthController::class,'update'])->middleware('auth:api');
